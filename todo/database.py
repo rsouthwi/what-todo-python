b0a30d03a0a419c7f66554ab83895108db781264
Ron Southwick <ronlsouthwick@gmail.com>
2023-02-07 16:01:09 -0500

    just some notes

--- todo/database.py
+++ todo/database.py
@@ -1,3 +1,9 @@
+"""
+the "database" should be pulled from the JSON file into memory as
+soon as possible and kept there through the entire session.
+all "writes/changes/deletes" should get to the JSON file immediately
+"""
+
 from pathlib import Path
 
 from todo import DB_WRITE_ERROR, SUCCESS
